9d6f8920d5876662febb9fced882dc36dc31cc28
yanpatla <tomipatlayan@gmail.com>
2026-02-23 11:16:13 +0200

    deleting unused import

--- src/components/ButtonGoogle.tsx
+++ src/components/ButtonGoogle.tsx
@@ -1,5 +1,4 @@
 import { useGoogleLogin } from "@/hooks/auth/useGoogleLogin";
-import React from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function ButtonGoogle() {
