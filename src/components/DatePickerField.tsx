e66ce512d8d1044dc000208155629a72573474a6
yanpatla <tomipatlayan@gmail.com>
2026-02-23 11:16:49 +0200

    deleting unused import

--- src/components/DatePickerField.tsx
+++ src/components/DatePickerField.tsx
@@ -4,9 +4,9 @@ import { Calendar as CalendarIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
 } from "@/components/ui/popover";
 
 type DatePickerFieldProps = {
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Toaster } from "./components/ui/sonner";
 import "./index.css";
 import Router from "./router/router";
-import { Toaster } from "./components/ui/sonner";
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
